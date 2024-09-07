37b9c062028150de178e2ed8c008f90667929395
IEVN1003-19002450 <beto.pandabox@gmail.com>
2024-09-06 18:58:43 -0600

    Primer commit del proyecto

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,22 @@
+var num1=38
+var num2=29
+const num3=10
+
+let sum=num1+num2+num3;
+
+let nombre:string
+let num:number
+let activo:Boolean
+
+
+nombre='Isaac'
+num=11
+activo=true
+
+let matricula:number|string|boolean
+
+matricula=19002450
+matricula='asdfghjkl'
+
+
+console.log()
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,11 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+const aumno:Alumno={
+    nombre:'Beto',
+edad:24,
+email:'beto@gmail.com',
+}
